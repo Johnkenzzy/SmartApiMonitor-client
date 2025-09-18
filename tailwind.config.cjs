a590637d1cdf000d7fc717d253546a9fdf9f9e39
Johnkenzzy <johnkenumeh@gmail.com>
2025-09-18 12:35:14 +0100

    setup: tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#A3E635", // lemon green
+          dark: "#84CC16", // darker variant
+          light: "#BEF264", // lighter variant
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
