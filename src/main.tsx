ede1bea1c3f3902e1cd4882937dace5e88ea11d8
Johnkenzzy <johnkenumeh@gmail.com>
2025-09-18 14:46:25 +0100

    setup: root .html and .css files

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // ⚡ don’t spam refresh on tab switch
+      retry: 1, // retry failed requests once
+      staleTime: 1000 * 60, // cache valid for 1min
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
